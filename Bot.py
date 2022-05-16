fe78c853c7af3d67dd86b68ff8b792f679196a62
Luxo-beep <103624069+Luxo-beep@users.noreply.github.com>
2022-05-16 18:10:55 +0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -17,35 +17,35 @@ def get_text_messages(message):
  if(message.text == "❓ Задать вопрос"):
    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
    bot.send_message(message.chat.id, text="Hапиши @votsi_ken если хочешь получить помощь", reply_markup=markup)
- if message.text == "123456":
+ if message.text == "2284":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/1.xlsx', 'rb'))
- if message.text == "918827":
+ if message.text == "280990":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/2.xlsx', 'rb'))
- if message.text == "243751":
+ if message.text == "arisha34030":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/3.xlsx', 'rb'))
- if message.text == "258185":
+ if message.text == "27ko4ergin27":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/4.xlsx', 'rb'))
- if message.text == "954321":
+ if message.text == "7777777":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/5.xlsx', 'rb'))
- if message.text == "145560":
+ if message.text == "foom.naa15":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/6.xlsx', 'rb'))
- if message.text == "326561":
+ if message.text == "dora232919":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/7.xlsx', 'rb'))
- if message.text == "890752":
+ if message.text == "Dashka":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/8.xlsx', 'rb'))
- if message.text == "662604":
+ if message.text == "marrie669":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/9.xlsx', 'rb'))
- if message.text == "856067":
+ if message.text == "Artem1":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/10.xlsx', 'rb'))
- if message.text == "653790":
+ if message.text == "Artem2":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/11.xlsx', 'rb'))
- if message.text == "716423":
+ if message.text == "Andrey1":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/12.xlsx', 'rb'))
- if message.text == "293698":
+ if message.text == "Andrey2":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/13.xlsx', 'rb'))
- if message.text == "537479":
+ if message.text == "18963LmirryCM":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/14.xlsx', 'rb'))
- if message.text == "345176":
+ if message.text == "Testcode":
     bot.send_document(message.from_user.id, document=open('/evp-bot/1/15.xlsx', 'rb'))
  elif message.text == "/help":
     bot.send_message(message.from_user.id, "Напиши @votsi_ken если есть вопросы")
